264032b8f0210ee942afd031a83bb4753468942e
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-06-25 05:07:17 +0530

    initial commit

---
+++ src/main/java/com/microservices/springssogoogle/SpringSsoGoogleApplication.java
@@ -0,0 +1,29 @@
+package com.microservices.springssogoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class SpringSsoGoogleApplication {
+
+    @GetMapping
+    public String welcome() {
+        return "welcome to google!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username: " + principal.getName());
+        return principal;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsoGoogleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservices/springssogoogle/SpringSsoGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.springssogoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
